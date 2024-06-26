7596b57a27a7061a0089d01204a0f357ba423e46
Nidhi1shah <74060548+Nidhi1shah@users.noreply.github.com>
2024-06-26 13:52:49 +0100

    testing get 100 records with EORI with 6.0000 load

--- src/test/scala/uk/gov/hmrc/perftests/TGP/TGPAPISimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/TGP/TGPAPISimulation.scala
@@ -63,9 +63,9 @@ class TGPAPISimulation extends PerformanceTestRunner {
     "GET (100) Records by Page,Size,Date,EORI - Success Response 200"
   )
     .withRequests(
-      get100GoodsRecordsByPage,
-      get100GoodsRecordsBySize,
-      get100GoodsRecordsByDate,
+//      get100GoodsRecordsByPage,
+//      get100GoodsRecordsBySize,
+//      get100GoodsRecordsByDate,
       get100GoodsRecordsByEORI
     )
 
