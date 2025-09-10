2a1a60362de078774469f1f115a20fb0ee8fd932
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-09-10 13:46:14 +0100

    Tgp 3571 bobby rules (#65)
    * TGP-3571 Bobby Rules updates
    
    * TGP-3571 revert dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "performance-test-runner" % "6.2.0",
+    "uk.gov.hmrc"       %% "performance-test-runner" % "6.1.0",
     "org.playframework" %% "play-ahc-ws-standalone"  % "3.0.5",
     "org.playframework" %% "play-ws-standalone-json" % "3.0.5",
     "org.apache.pekko"  %% "pekko-stream"            % "1.0.3",
